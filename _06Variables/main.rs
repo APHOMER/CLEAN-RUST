540824a91719ca5d4cae59f518766366b58c90a1
Aphomer <aaphomer@gmail.com>
2025-07-08 18:34:10 +0100

    worked on variables and mutability

---
+++ _06Variables/main.rs
@@ -0,0 +1,11 @@
+
+// VARIABLES AND MUTABILITY IN RUST
+fn main() {
+    println!("Hello Bro Mercy.");
+    let mut a: i32 = 5;
+    println!("The value of a is {}", a);
+    a =  10;
+    println!("The value of a is now {}", a);
+}
+
+
